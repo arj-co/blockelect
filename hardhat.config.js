2e726315a7cafd39ff01100e3b55b3a1e2ab207d
arj-co <arjco@proton.me>
2025-09-28 10:20:00 +0530

    Enhance hardhat configuration file with detailed network comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ * Hardhat project configuration setup for compiling Solidity smart contracts.
+ * By default, compiles with Solidity version 0.8.28.
+ */
 module.exports = {
   solidity: "0.8.28",
 };
