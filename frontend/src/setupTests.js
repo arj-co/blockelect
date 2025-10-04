8cf4a6e9ac68108a5132feb62d2e14e001726777
arj-co <arjco@proton.me>
2025-10-04 09:45:00 +0530

    Document testing environment configuration for jest-dom matchers

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// Immutably hooks into jest's global expect object before each test file runs.
 import '@testing-library/jest-dom';
